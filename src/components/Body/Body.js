651ad62f8e2a1a913190eb119f6f4cc347628a98
Joey Gaitan <joeygaitan707@gmail.com>
2019-06-19 23:02:37 -0700

    working on getting the body information  set up

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -20,13 +20,8 @@ class Body extends Component {
                     <p>Express.js (MVC)</p>
                 </div>  
                 <div className="innerContainer">
-                    <div className="item1">
                         <h2>Intro</h2>
                         <h4>I started my way into the tech field when I was in the sixth grade learning about how to making game servers. After trying to teach myself I decided to go an organized way of learning, so I attended a Junior College with the plan to acquire a Computer Science Degree. However, while In College I decided I wanted more of a focused path for learning computer science. That’s when I decided to apply to coding bootcamps. I eventually got into one and started attending Galvanize in Seattle. The program focused on learning about web development and some computer science fundamentals. It taught me so many skills such as working with other software engineers, writing readable code, how to focus my attention on the right problems, how to ask the right questions, asking for help when I need it, and most of all how to teach myself to learn new concepts on my own. Learning, problem-solving, and computers have always been my passions, and I hope to never stop pursuing them.</h4>
-                    </div>
-                    <div className="item2">
-
-                    </div>
                 </div>
             </div>
         </div> 
