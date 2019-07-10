9940e1a3cc5434ab6b68731a714235f62141b739
Joey Gaitan <joeygaitan707@gmail.com>
2019-07-09 19:38:50 -0700

    got the svg partially working

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -11,12 +11,13 @@ class NavBar extends Component {
     render() { 
         return ( 
         <div className="container">
+            <img className="dot" src={require("./dot.svg")}/>
             <h3>Home</h3>
             <h3>Skills</h3>
             <h3>About</h3>
             <h3>Work</h3>
             <h3>Contact</h3>
-            <img alt=""src={"./dot.svg"}/>
+            
         </div> );
     }
 }
