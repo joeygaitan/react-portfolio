5572ad4103cee7a6f0f9d088ba07a68d231bb006
Joey Gaitan <joeygaitan707@gmail.com>
2019-07-16 18:45:38 -0700

    working with svg still :( trying to get the svgs working and learn affinity desinger

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-// import './css/app.css'
 import NavBar from './components/Navbar/NavBar'
 import Header from './components/Header/Header';
 import Body from './components/Body/Body';
 import Projects from './components/projects/Projects';
 import Footer from './components/Footer/Footer';
+
 import './css/app.css'
 class App extends Component {
   render() {
