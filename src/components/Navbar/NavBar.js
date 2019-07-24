10799e8a063c5dc36865e39c3428cab35f325e78
Joey Gaitan <joeygaitan707@gmail.com>
2019-07-23 20:06:49 -0700

    got the svg to almost work properly

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import '../../css/navbar.css'
 import { ReactComponent as Dot } from './dot.svg';
 
+
 class NavBar extends Component {
     constructor(props) {
         super(props);
@@ -12,7 +13,6 @@ class NavBar extends Component {
     render() { 
         return ( 
         <div className="container">
-            
             <div className="home">
                 <Dot className="dotSvg"/>
                 <h3>Home</h3>
