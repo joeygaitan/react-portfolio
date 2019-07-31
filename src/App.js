8015ba94eb100a0e4148594ea7790d16b6c728be
Joey Gaitan <joeygaitan707@gmail.com>
2019-07-30 18:37:38 -0700

    navbar is semi functional now

--- src/App.js
+++ src/App.js
@@ -7,6 +7,46 @@ import Footer from './components/Footer/Footer';
 
 import './css/app.css'
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+
+    }
+    this.home = React.createRef();
+    this.skills = React.createRef();
+    this.about = React.createRef();
+    this.work = React.createRef();
+    this.contact = React.createRef();
+  } 
+  
+  scrollToRef = ref => {
+    if (ref === 'home')
+      window.scrollTo({
+        top: this.home.current.offsetTop,
+        behavior: 'smooth'
+      });
+    if (ref === 'skills')
+      window.scrollTo({
+        top: this.skills.current.offsetTop,
+        behavior: 'smooth'
+      });
+    if (ref === 'about')
+      window.scrollTo({
+        top: this.about.current.offsetTop,
+        behavior: 'smooth'
+      });
+    if (ref === 'work')
+      window.scrollTo({
+        top: this.work.current.offsetTop,
+        behavior: 'smooth'
+      });
+    if (ref === 'contact')
+      window.scrollTo({
+        top: this.contact.current.offsetTop,
+        behavior: 'smooth'
+      });
+  };
+
   render() {
     return (
       <div id="wrapper">
--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -9,40 +9,8 @@ class NavBar extends Component {
         this.state={
 
         }
-        this.home = React.createRef();
-        this.skills = React.createRef();
-        this.about = React.createRef();
-        this.work = React.createRef();
-        this.contact = React.createRef();
     }
 
-    scrollToRef = ref => {
-        if (ref === 'home')
-          window.scrollTo({
-            top: this.home.current.offsetTop,
-            behavior: 'smooth'
-          });
-        if (ref === 'skills')
-          window.scrollTo({
-            top: this.skills.current.offsetTop,
-            behavior: 'smooth'
-          });
-        if (ref === 'about')
-          window.scrollTo({
-            top: this.about.current.offsetTop,
-            behavior: 'smooth'
-          });
-        if (ref === 'work')
-          window.scrollTo({
-            top: this.work.current.offsetTop,
-            behavior: 'smooth'
-          });
-        if (ref === 'contact')
-          window.scrollTo({
-            top: this.contact.current.offsetTop,
-            behavior: 'smooth'
-          });
-      };
 
     
     render() { 
@@ -52,25 +20,25 @@ class NavBar extends Component {
             
             </div>
             <div className="Buttons">
-                <div className="home">
+                <div className="buttonItem">
                     <Dot className="dotSvg"/>
-                    <h3>Home</h3>
+                    <h4 className="buttonText">Home</h4>
                 </div>
-                <div className="skills">
+                <div className="buttonItem">
                     <Dot className="dotSvg"/>
-                    <h3>Skills</h3>
+                    <h4 className="buttonText">Skills</h4>
                 </div>
-                <div className="about">
+                <div className="buttonItem">
                     <Dot className="dotSvg"/>
-                    <h3>About</h3>
+                    <h4 className="buttonText">About</h4>
                 </div>
-                <div>
+                <div className="buttonItem">
                     <Dot className="dotSvg"/>
-                    <h3>Work</h3>
+                    <h4 className="buttonText">Work</h4>
                 </div>
-                <div>
+                <div className="buttonItem">
                     <Dot className="dotSvg"/>
-                    <h3>Contact</h3>
+                    <h4 className="buttonText">Contact</h4>
                 </div>
             </div>
         </div> );
