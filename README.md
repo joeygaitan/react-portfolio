60bc4cdcb05d2ddde14cfd39e018985a029d992e
Joey Gaitan <joeygaitan707@gmail.com>
2019-09-04 21:54:19 -0700

    finished the self portrait drawing. Took forever to do however.  I learned about using affinity desinger and it was a lot of fun

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ###Personal Portfolio
+* Using React.js, React Router, CSS, and Google Firebase
 
-*Using React.js, React Router, CSS, and Google Firebase
-
-###1
-setting up the body
+###User Stories
+* As a user I want to be able to read the portfolio quickly and have a seamless flow through the page displaying who the person is, the skills, a little bio, personal projects, experience, and education.
--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -18,10 +18,14 @@ class Body extends Component {
                     <p>CSS</p>
                     <p>AJAX</p>
                     <p>Express.js (MVC)</p>
-                </div>  
+                </div>
                 <div className="innerContainer" ref={this.props.refAbout}>
-                        <h2>Intro</h2>
-                        <h4>I started my way into the tech field when I was in the sixth grade learning about making game servers. After trying to teach myself I decided to go an organized way of learning, so I attended a Junior College with the plan to acquire a Computer Science Degree. However, while In College I decided I wanted more of a focused path for learning computer science. That’s when I decided to apply to coding bootcamps. I eventually got into one and started attending Galvanize in Seattle. The program focused on learning about web development and some computer science fundamentals. It taught me so many skills such as working with other software engineers, writing readable code, how to focus my attention on the right problems, how to ask the right questions, asking for help when I need it, and most of all how to teach myself to learn new concepts on my own. Learning, problem-solving, and computers have always been my passions, and I hope to never stop pursuing them. I am now attending the Make School In San Francisco getting a Bachelors Degree In applied Computer Science learning IOS development, and Fullstack Development.</h4>
+                        <div className="education">
+                            <h2>Intro</h2>
+                        </div>
+                        <div className="backStory">
+                            <h4>I started my way into the tech field when I was in the sixth grade learning about making game servers. After trying to teach myself I decided to go an organized way of learning, so I attended a Junior College with the plan to acquire a Computer Science Degree. However, while In College I decided I wanted more of a focused path for learning computer science. That’s when I decided to apply to coding bootcamps. I eventually got into one and started attending Galvanize in Seattle. The program focused on learning about web development and some computer science fundamentals. It taught me so many skills such as working with other software engineers, writing readable code, how to focus my attention on the right problems, how to ask the right questions, asking for help when I need it, and most of all how to teach myself to learn new concepts on my own. Learning, problem-solving, and computers have always been my passions, and I hope to never stop pursuing them. I am now attending the Make School In San Francisco getting a Bachelors Degree In applied Computer Science learning IOS development, and Fullstack Development.</h4>
+                        </div>
                 </div>
             </div>
          );
